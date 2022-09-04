72b8666524e86009cef811e8479f85f1d49f58a6
rajaryan0528 <rajaryan0528@gmail.com>
2022-09-04 16:06:14 +0530

    Some changes

--- singlyLL.cpp
+++ singlyLL.cpp
@@ -4,58 +4,101 @@
 (iii) Insert an element x at i th  position in the singly linked list
 (iv) Remove an element from the beginning of the singly linked list
 (iv) Remove an element from the end of the singly linked list
-(v) Remove an element from  i th  position in the singly linked list. 
+(v) Remove an element from  i th  position in the singly linked list.
 (vi) Search for an element x in the singly linked list and return its pointer
 (vii) Concatenate two singly linked lists
 (vii)Reverse a  singly linked list
 */
 #include <iostream>
 using namespace std;
-class node 
+class node
 {
-    public:
-      int data;
-      node* next;
+public:
+    node()
+    {
+    }
+    int data;
+    node *next;
 };
 
 class sll
 {
-  node *first=NULL;
-  node *temp =NULL;
-  node *last=NULL;
-//   sll()
-//   {
-//     temp=new node();
-//     temp->data=0;
-//     temp->next=NULL;
-//   }
+    node *first = NULL;
+    node *temp = NULL;
+    node *last = NULL;
+    node *iter = NULL;
 
-//   sll(int val)
-//   {
-//       temp = new node();
-//       temp->data = val;
-//       temp->next = NULL;
-//       head=last=temp;
-//   }
 public:
-  void display(node *n);
-  void add_to_head(int v);
-  void add_to_tail(int);
-  void add_at(int index);
-  void del_from_head();
-  void del_from_tail();
-  void delete_from(int index);
+    sll(){};
+    ~sll(){};
+    void display();
+    int count();
+    void add_to_head(int v);
+    void add_to_tail(int);
+    void add_at_loc(int i, int e);
+    void del_from_head();
+    void del_from_tail();
+    void del_from_loc(int i);
+    void search(int );
+    void concatenate();
+    void reverse();
 };
 
-void sll::display(node *n)
+//feedback for element  being not  present
+void sll::search(int e)
 {
-    while(n !=NULL)
+    temp = first;
+    int count =1;
+    while (temp->data != e)
     {
-        cout<<n->data<<"->";
-        n=n->next;
+        temp = temp->next;
+        count+=1;
+    }
+    cout<<"Element present at position :"<<count<<endl;
+}
+
+int sll::count()
+{
+    int count = 0;
+    node *temp = first;
+    while (temp != NULL)
+    {
+        count += 1;
+        temp = temp->next;
+    }
+
+    return count;
+}
+void sll::display()
+{
+    node *temp = first;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void sll::add_to_tail(int v)
+{
+
+    temp = new node();
+    temp->data = v;
+    temp->next = NULL;
+    if (first == NULL)
+    {
+        first = last = temp;
+    }
+
+    else
+    {
+        last->next = temp;
+        last = temp;
     }
 }
-void sll::add_to_tail(int v)
+
+void sll::add_to_head(int v)
 {
     temp = new node();
     temp->data = v;
@@ -63,65 +106,127 @@ void sll::add_to_tail(int v)
     if (first == NULL)
     {
         first = last = temp;
-     }
+    }
 
-     else{
-          last->next=temp;
-          last=temp;
-     }
-
-    display(first);
-
-
+    else
+    {
+        temp->next = first;
+        first = temp;
+    }
 }
 
-void sll::add_to_head(int v)
+void sll::del_from_head()
 {
-    
-    temp->data = v;
-    temp->next = NULL;
+    temp = new node();
     if (first == NULL)
     {
-        first = last = temp;
-     }
+        cout<<"List is empty. Cannot delete:)"<<endl;
 
-     else{
-          temp->next=first;
-          first=temp;
-     }
+    }
 
-     display(first);
+    else
+    {
+        temp=first;
+        first=first->next;
+        delete temp;
+    }
 }
 
+void sll::del_from_tail()
+{
+    temp = new node();
+    node *current=NULL;
+
+    if (first == NULL)
+    {
+        cout << "List is empty. Cannot delete:)" << endl;
+    }
+
+    else
+    {
+
+        temp = first;
+        while(temp->next!=NULL)
+        {
+           current=temp;  //storing the previous node
+           temp = temp->next;
+        }
+        
+        //temp is storing the last node here
+        current->next = NULL; // becomes the last node
+        delete temp;
+    }
+}
+
+void sll::del_from_loc(int i)
+{
+    // i is position
+    //delete the  element at the i th position
+    temp=new node();
+    iter = first;
+    if (i == 1)
+        this->del_from_head();
+
+    else if (i == (this->count()))
+        this->del_from_tail();
+
+    else
+    {
+        int count = 1;
+        iter = first;
+        while (count < i)
+        {
+             
+            temp=iter; //previous node
+            iter = iter->next;
+            count += 1;
+        }
+        temp->next=iter->next; //point to the address of the node after the deleted node 
+        delete iter;     
+    }
+}
+
+void sll::add_at_loc(int i, int e)
+{
+    // i is position
+    // add an element agter the ith position 
+    temp = new node();
+    temp->data = e;
+    temp->next = NULL;
+    iter = first;
+    if (i == 1)
+        this->add_to_head(e);
+
+    else if (i == (this->count()))
+    {
+        this->add_to_tail(e);
+    }
+
+    else
+    {
+        int count = 1;
+        iter = first;
+        while (count < i)
+        {
+           
+            iter = iter->next;
+            count += 1;
+        }
+        temp->next = iter->next;
+        iter->next = temp;
+    }
+}
 
 int main()
 {
-   sll list;
-   list.add_to_tail(10);
-   list.add_to_tail(30);
-
-
-
-
-    // node *head = NULL;
-    // node *second = NULL;
-    // node *third = NULL;
-
-    // allocate 3 nodes in the heap
-    // head = new node();
-    // second = new node();
-    // third = new node();
-
-    
-    // head->data = 1;
-    // head->next = second;
-    // second->data = 2;
-    // second->next = third;
-
-    // third->data = 3;
-    // third->next = NULL;
-
-
-
+    sll list;
+    list.add_to_tail(10);
+    list.add_to_tail(30);
+    list.add_to_tail(50);
+    //list.del_from_loc(3); // i, e
+    //list.del_from_tail();
+    //list.del_from_head();
+    list.display();
+    list.search(30);
     return 0;
 }
